33f586b57af3e42fe015d6c5daf57c1b3398ce37
ESTUDIANTE <ESTUDIANTE@GGEDCLABWRK064>
2016-11-30 13:47:36 -0500

    Listoszdjgvnkb

---
+++ Avance_Proyecto/Avance_Proyecto/Candidato.vb
@@ -0,0 +1,32 @@
+﻿Public Class Candidato
+    Inherits Persona
+    Private _usuarioCandidato As String
+    Public Property UsuarioCandidato() As String
+        Get
+            Return _usuarioCandidato
+        End Get
+        Set(ByVal value As String)
+            _usuarioCandidato = value
+        End Set
+    End Property
+
+    Private _claveCandidato As String
+    Public Property ClaveCandidato() As String
+        Get
+            Return _claveCandidato
+        End Get
+        Set(ByVal value As String)
+            _claveCandidato = value
+        End Set
+    End Property
+
+    Private _dignidad As String
+    Public Property Dignidad() As String
+        Get
+            Return _dignidad
+        End Get
+        Set(ByVal value As String)
+            _dignidad = value
+        End Set
+    End Property
+End Class
---
+++ Avance_Proyecto/Avance_Proyecto/Votante.vb
@@ -0,0 +1,14 @@
+﻿Public Class Votante
+    Inherits Persona
+
+    Private _estado As Boolean
+    Public Property Estado() As Boolean
+        Get
+            Return _estado
+        End Get
+        Set(ByVal value As Boolean)
+            _estado = value
+        End Set
+    End Property
+
+End Class
