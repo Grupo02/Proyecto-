25d5e74dc9e15f0e8139cc1d6fdabd365d572839
jowpris <rickcolla@hotmail.com>
2016-11-30 13:39:21 -0500

    Crear clases

---
+++ Avance_Proyecto/Avance_Proyecto/Administrador.vb
@@ -0,0 +1,23 @@
+﻿Public Class Administrador
+
+    Private _usuarioAdministrado As String
+    Public Property UsuarioAdministrador() As String
+        Get
+            Return _usuarioAdministrado
+        End Get
+        Set(ByVal value As String)
+            _usuarioAdministrado = value
+        End Set
+    End Property
+
+    Private _claveAdministrador As String
+    Public Property ClaveAdministrador() As String
+        Get
+            Return _claveAdministrador
+        End Get
+        Set(ByVal value As String)
+            _claveAdministrador = value
+        End Set
+    End Property
+
+End Class
---
+++ Avance_Proyecto/Avance_Proyecto/Persona.vb
@@ -0,0 +1,42 @@
+﻿Public Class Persona
+
+    Private _id As Integer
+    Public Property Id() As Integer
+        Get
+            Return _id
+        End Get
+        Set(ByVal value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Private _nombre As String
+    Public Property Nombre() As String
+        Get
+            Return _nombre
+        End Get
+        Set(ByVal value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Private _apellido As String
+    Public Property Apellido() As String
+        Get
+            Return _apellido
+        End Get
+        Set(ByVal value As String)
+            _apellido = value
+        End Set
+    End Property
+
+    Private _edad As Integer
+    Public Property Edad() As Integer
+        Get
+            Return _edad
+        End Get
+        Set(ByVal value As Integer)
+            _edad = value
+        End Set
+    End Property
+End Class
