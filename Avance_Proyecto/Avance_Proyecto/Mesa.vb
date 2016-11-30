3ca5811933b2c7a9e8628d8ee13a227a2c0cc9e3
ESTUDIANTE <ESTUDIANTE@GGEDCLABWRK063>
2016-11-30 13:33:13 -0500

    Actualizacion 2

--- Avance_Proyecto/Avance_Proyecto/Mesa.vb
+++ Avance_Proyecto/Avance_Proyecto/Mesa.vb
@@ -8,4 +8,23 @@
             _id = value
         End Set
     End Property
+    Private _estado As Boolean
+    Public Property Estado() As Boolean
+        Get
+            Return _estado
+        End Get
+        Set(ByVal value As Boolean)
+            _estado = value
+        End Set
+    End Property
+
+    Private _votantes As ArrayList
+    Public Property Votantes() As ArrayList
+        Get
+            Return _votantes
+        End Get
+        Set(ByVal value As ArrayList)
+            _votantes = value
+        End Set
+    End Property
 End Class
---
+++ Avance_Proyecto/Avance_Proyecto/Voto.vb
@@ -0,0 +1,11 @@
+﻿Public Class Voto
+    Private _candidato As Candidato
+    Public Property Candidato() As Candidato
+        Get
+            Return _candidato
+        End Get
+        Set(ByVal value As Candidato)
+            _candidato = value
+        End Set
+    End Property
+End Class
