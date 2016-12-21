f813781be3fb4f45ec8eb1fa405a9e67d12eb7e7
jowpris <rickcolla@hotmail.com>
2016-12-20 22:42:20 -0500

    id de la papeleta

--- Avance_Proyecto/Avance_Proyecto/Voto.vb
+++ Avance_Proyecto/Avance_Proyecto/Voto.vb
@@ -1,4 +1,14 @@
 ﻿Public Class Voto
+    Private _id As Int16
+    Public Property Id() As Int16
+        Get
+            Return _id
+        End Get
+        Set(ByVal value As Int16)
+            _id = value
+        End Set
+    End Property
+
     Private _candidato As Candidato
     Public Property Candidato() As Candidato
         Get
