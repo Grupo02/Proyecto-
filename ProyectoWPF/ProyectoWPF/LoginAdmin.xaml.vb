c83bfb211eff3a39f62e9402c04ae829d7ad02d3
GiPaolaOrtiz <paogi_ortiz@hotmail.com>
2017-01-27 12:42:32 -0500

    Login-Correcion

--- ProyectoWPF/ProyectoWPF/LoginAdmin.xaml.vb
+++ ProyectoWPF/ProyectoWPF/LoginAdmin.xaml.vb
@@ -3,7 +3,7 @@ Imports System.Data
 Public Class LoginAdmin
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        Dim dbPath = "C:\Users\PaolaO\Documents\ESPOL\5S\Programación Visual\Ejercicios\BD\sample.mdb"
+        Dim dbPath = "E:\VISUAL BASIC\Proyecto_Visual.mdb"
         Dim strConexion = "Provider=Microsoft.Jet.OLEDB.4.0; " &
             "Data Source=" & dbPath
         'Dim dbConexion As New OleDbConnection(strConexion)
@@ -14,8 +14,6 @@ Public Class LoginAdmin
 
         Using dbConexion As New OleDbConnection(strConexion)
             'Console.WriteLine("Conexion exitosa")
-            Dim strQuery As String = "SELECT * FROM tbl_master"
-            Dim dbAdapter As New OleDbDataAdapter(strQuery, dbConexion)
 
 
         End Using
@@ -23,14 +21,14 @@ Public Class LoginAdmin
     End Sub
     Private Sub bntIngresarAdmin_Click(sender As Object, e As RoutedEventArgs) Handles bntIngresarAdmin.Click
 
-        Dim dbPath = "C:\Users\PaolaO\Documents\ESPOL\5S\Programación Visual\Ejercicios\BD\sample.mdb"
+        Dim dbPath = "E:\VISUAL BASIC\Proyecto_Visual.mdb"
         Dim strConexion = "Provider=Microsoft.Jet.OLEDB.4.0; " &
             "Data Source=" & dbPath
         Dim dbConexion As New OleDbConnection(strConexion)
 
         dbConexion.Open()
         Console.WriteLine("Conexion exitosa")
-        Dim strQuery1 As String = "SELECT firstName, LastName FROM tbl_master WHERE firstName ='" & txtUserAdmin.Text & "'AND LastName ='" & txtClaveAdmin.Text & "'"
+        Dim strQuery1 As String = "SELECT usuario, clave FROM Administrador WHERE usuario ='" & txtUserAdmin.Text & "'AND clave ='" & passAdmin.Password & "'"
         Dim comandos = New OleDbCommand(strQuery1, dbConexion)
         Dim adapter As New OleDbDataAdapter
         adapter.SelectCommand = comandos
---
+++ ProyectoWPF/ProyectoWPF/VentanaCandidato.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class VentanaCandidato
+
+End Class
