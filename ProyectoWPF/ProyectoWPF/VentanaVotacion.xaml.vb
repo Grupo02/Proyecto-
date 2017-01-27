16ff2062de93322b07ba3c1020f56391df2fdfee
Walter Fabre <walterfa@outlook.es>
2017-01-27 07:04:29 -0500

    eventos referenciales

--- ProyectoWPF/ProyectoWPF/VentanaVotacion.xaml.vb
+++ ProyectoWPF/ProyectoWPF/VentanaVotacion.xaml.vb
@@ -1,3 +1,37 @@
 ﻿Public Class VentanaVotacion
+    Private Sub btn_op1_p_Click(sender As Object, e As RoutedEventArgs) Handles btn_op1_p.Click
 
+    End Sub
+
+    Private Sub btn_op2_p_Click(sender As Object, e As RoutedEventArgs) Handles btn_op2_p.Click
+
+    End Sub
+
+    Private Sub btn_op3_p_Click(sender As Object, e As RoutedEventArgs) Handles btn_op3_p.Click
+
+    End Sub
+
+    Private Sub btn_op1_a_Click(sender As Object, e As RoutedEventArgs) Handles btn_op1_a.Click
+
+    End Sub
+
+    Private Sub btn_op2_a_Click(sender As Object, e As RoutedEventArgs) Handles btn_op2_a.Click
+
+    End Sub
+
+    Private Sub btn_op3_a_Click(sender As Object, e As RoutedEventArgs) Handles btn_op3_a.Click
+
+    End Sub
+
+    Private Sub btn_op1_c_Click(sender As Object, e As RoutedEventArgs) Handles btn_op1_c.Click
+
+    End Sub
+
+    Private Sub btn_op2_c_Click(sender As Object, e As RoutedEventArgs) Handles btn_op2_c.Click
+
+    End Sub
+
+    Private Sub btn_op3_c_Click(sender As Object, e As RoutedEventArgs) Handles btn_op3_c.Click
+
+    End Sub
 End Class
