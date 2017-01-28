dfce77b7666c9905bd87c712e6cba6a7ae1d946b
jowpris <rickcolla@hotmail.com>
2017-01-27 23:12:10 -0500

    Ventanas formularios

---
+++ ProyectoWPF/ProyectoWPF/VentanaAgregarCandidato.xaml.vb
@@ -0,0 +1,7 @@
+﻿Public Class VentanaAgregarCandidato
+    Private Sub cBoxDignidad_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cBoxDignidad.SelectionChanged
+        cBoxDignidad.Items.Add("Presidente")
+        cBoxDignidad.Items.Add("Asambleísta")
+        cBoxDignidad.Items.Add("Alcalde")
+    End Sub
+End Class
---
+++ ProyectoWPF/ProyectoWPF/VentanaEliminarVotante.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class VentanaEliminarVotante
+
+End Class
