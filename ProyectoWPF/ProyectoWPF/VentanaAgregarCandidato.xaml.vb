4a964ccf96def679a772ac077f80a1c27c31abdd
jowpris <rickcolla@hotmail.com>
2017-01-31 17:14:53 -0500

    Agregar Candidato

--- ProyectoWPF/ProyectoWPF/VentanaAgregarCandidato.xaml.vb
+++ ProyectoWPF/ProyectoWPF/VentanaAgregarCandidato.xaml.vb
@@ -1,7 +1,69 @@
-﻿Public Class VentanaAgregarCandidato
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class VentanaAgregarCandidato
+
+    Private dbPath As String = "Proyecto_Visual.mdb"
+    Private strConexion As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dbPath
+
+    Private dsCandidato As DataSet
     Private Sub cBoxDignidad_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cBoxDignidad.SelectionChanged
         cBoxDignidad.Items.Add("Presidente")
         cBoxDignidad.Items.Add("Asambleísta")
         cBoxDignidad.Items.Add("Alcalde")
+    End Sub
+
+    Private Sub btnGuardarCandidato_Click(sender As Object, e As RoutedEventArgs) Handles btnGuardarCandidato.Click
+
+
+        UpdatePersona(txtCedulaCandidato.Text, txtNombresCandidato.Text, txtApellidosCandidato.Text, txtUsuarioCandidato.Text, passClaveUser.Password, cBoxDignidad.Text, txtListaCandidato.Text)
+        MsgBox("Candidato Guardado")
+        Me.Close()
+    End Sub
+
+    Public Sub UpdatePersona(id As Integer, nombre As String, apellido As String, usuario As String, clave As String, dignidades As String, listaCandidato As Integer)
+
+
+        If dignidades = "Presidente" Then
+            Me.dsCandidato.Tables("Candidato").Rows.Add(id, usuario, clave, dignidades, nombre, apellido, 0, listaCandidato, 0)
+        End If
+            If dignidades = "Asambleista" Then
+            Me.dsCandidato.Tables("Candidato").Rows.Add(id, usuario, clave, dignidades, nombre, apellido, 0, listaCandidato, 0)
+        End If
+            If dignidades = "Prefecto" Then
+            Me.dsCandidato.Tables("Candidato").Rows.Add(id, usuario, clave, dignidades, nombre, apellido, 0, listaCandidato, 0)
+        End If
+
+        Using conexion As New OleDbConnection(strConexion)
+            Dim consulta As String = "Select * FROM Candidato;"
+            'Dim adapter As New OleDbDataAdapter(consulta, conexion)
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            'adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+
+            Try
+                adapter.Update(dsCandidato.Tables("Candidato"))
+            Catch ex As Exception
+                MessageBox.Show("Error al guardar")
+            End Try
+
+        End Using
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Using conexion As New OleDbConnection(strConexion)
+
+            Dim consulta As String = "Select * FROM Candidato;"
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            dsCandidato = New DataSet("Candidato")
+            adapter.FillSchema(dsCandidato, SchemaType.Source)
+
+            adapter.Fill(dsCandidato, "Candidato")
+
+            'dataGridPersona.DataContext = dsPersonas
+
+        End Using
     End Sub
 End Class
